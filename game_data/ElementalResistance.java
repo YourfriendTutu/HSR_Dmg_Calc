974d22d6d518d764e8b34b399740528fda3f73f4
Derek <notrealyX@gmail.com>
2025-04-16 15:47:27 -0700

    Derek H. 4/17/2025 -Create ElementalResistance Class -Refactored files for organization -Created enemy_data database

---
+++ game_data/ElementalResistance.java
@@ -0,0 +1,47 @@
+package game_data;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ElementalResistance {
+    private String element;
+    private float resistance;
+    private static final ConcurrentHashMap<String, Float> elemResistances = new ConcurrentHashMap<>();
+
+    //Current playable Elements as of HSR V3.2
+    private static final List<String> elemsList = Arrays.asList("Physical", "Fire", "Ice", "Lightning", "Wind", "Quantum", "Imaginary");
+
+    static {
+        elemResistances.put("Physical", 0.0f);
+        elemResistances.put("Fire", 0.0f);
+        elemResistances.put("Ice", 0.0f);
+        elemResistances.put("Lightning", 0.0f);
+        elemResistances.put("Wind", 0.0f);
+        elemResistances.put("Quantum", 0.0f);
+        elemResistances.put("Imaginary", 0.0f);
+    }
+    //Getter for elemental resistance by element name
+    public static float getResistance(String element) {
+        return elemResistances.getOrDefault(element, 0.0f);
+    }
+    //Setter for elemental resistance
+    public static void setResistance(String element, float resistance) {
+        elemResistances.put(element, resistance);
+    }
+
+    //Setter for all resistances
+    public static void setAllResistances(Float resistance) {
+        for (String element : elemsList) {
+            elemResistances.put(element, resistance);
+        }
+    }
+
+    //Setter for default enemy res
+    public static void setEnemyRes() {
+        setAllResistances(0.2F);
+    }
+
+    //ToString
+
+}
---
+++ game_data/Enemies/EnemyCreator.java
@@ -0,0 +1,53 @@
+package game_data.Enemies;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.FileWriter;
+import java.lang.reflect.Type;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class EnemyCreator {
+
+    //Method to load enemies from JSON file
+    public static List<game_data.Enemies.Enemy> loadEnemies(String filename) {
+        try (FileReader reader = new FileReader(filename)) {
+            Type listType = new TypeToken<ArrayList<game_data.Enemies.Enemy>>() {}.getType();
+            Gson gson = new Gson();
+            return new Gson().fromJson(reader, listType);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
+    }
+
+    //Main Code
+    public static void main(String[] args) {
+        //Enemies are id from 1000-10000 Exclusive, by release version then alphabetically.
+
+        //V1.0
+        //Antibaryon
+        game_data.Enemies.Enemy antibaryon = new game_data.Enemies.Enemy(1001, "Antibaryon", 16429, 718, 1150, 109, 0.0F, Arrays.asList("Physical", "Quantum"));
+
+        //List of enemies to add to Json file
+        List<Enemy> enemies = new ArrayList<>();
+        enemies.add(antibaryon);
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        //Java to Json language converter
+        String enemyJson = gson.toJson(enemies);
+
+        //File writer to character_data.json
+        try (FileWriter writer = new FileWriter("game_data/Enemies/enemy_data.json")) {
+            writer.write(enemyJson);
+            System.out.println("Written updated enemy data to file.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
