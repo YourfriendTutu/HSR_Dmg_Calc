2ac047f0a96b564a93f241a6296d828488850874
grimm <sinogrimm@gmail.com>
2025-05-01 13:33:06 -0700

    starting gui layout

---
+++ DmgCalculator.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class DmgCalculator {
+    private JPanel contentPane;
+    private JTextField characterName;
+    private JTextField hpStat;
+    private JTextField atkStat;
+    private JTextField defStat;
+    private JTextField spdStat;
+    private JTextField crStat;
+    private JTextField cdStat;
+    private JTextField element;
+    private JTextField rarity;
+    private JTextField path;
+}
