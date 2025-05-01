7b289814d28fbbfe8dabb7e0f65a3786b60b3dd0
grimm <sinogrimm@gmail.com>
2025-05-01 12:16:39 -0700

    Update Lightcone.java

--- game_data/Lightcones/Lightcone.java
+++ game_data/Lightcones/Lightcone.java
@@ -2,6 +2,7 @@ package game_data.Lightcones;
 
 import game_data.Characters.Character;
 import game_data.Utility_Classes.Entity;
+import game_data.Utility_Classes.Enums.StatTypes;
 
 public class Lightcone extends Entity {
     private String path;
@@ -14,6 +15,14 @@ public class Lightcone extends Entity {
         this.path = path;
         this.rarity = rarity;
     }
+
+    //get Methods
+    public String getPath() {return this.path;}
+    public int getRarity() {return this.rarity;}
+
+    //set Methods
+    public void setPath(String path) {this.path = path;}
+    public void setRarity(int rarity) {this.rarity = rarity;}
 
     //Method to add LC stats to Character stats
     public static void equipLC(game_data.Characters.Character c, Lightcone lc) {
@@ -48,4 +57,19 @@ public class Lightcone extends Entity {
         c.setEquippedLightcone(null);
     }
 
+    @Override
+    public String printStats(){
+        return String.format("Lightcone Stats:\n"
+                        + "Name: %s\n"
+                        + "Path: %s\n"
+                        + "HP: %d\n"
+                        + "ATK: %d\n"
+                        + "DEF: %d\n"
+                        + "SPD: %d\n"
+                        + "CRIT Rate: %f\n"
+                        + "CRIT Dmg: %f\n"
+                , getName(), getPath(), getStatValue(StatTypes.HP, Integer.class), getStatValue(StatTypes.ATK, Integer.class),
+                getStatValue(StatTypes.DEF, Integer.class), getStatValue(StatTypes.SPD, Integer.class), getStatValue(StatTypes.CRIT_RATE, Float.class),
+                getStatValue(StatTypes.CRIT_DMG, Float.class));
+    }
 }
